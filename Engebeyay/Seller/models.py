e7edf4e1e54ecd604e67d3bc7bbbf865ca5d48c8
AmirEth <amirdulla11@gmail.com>
2023-02-22 01:58:11 -0800

    constructing our models

--- Engebeyay/Seller/models.py
+++ Engebeyay/Seller/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-from Seller.models import Product
 import uuid
+# Create your models here.
 
 
-class Buyer(models.Model):
+class Seller(models.Model):
     username= models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
     first_name=models.CharField(max_length=200,blank=True, null=True)
     last_name=models.CharField(max_length=200,blank=True, null=True)
+    phone_number=models.IntegerField(max_length=455, blank=True, null=True)
     email=models.EmailField(max_length=500, blank=True, null=True)
     adress=models.CharField(max_length=200,blank=True, null=True)
     profile_image=models.ImageField(null=True,blank=True, 
                                     upload_to='profiles/', default='profiles/user-default.png')
+    verification_image=models.ImageField(null=True,blank=True, 
+                                    upload_to='profiles/')
 
     created=models.DateTimeField(auto_now=True)
     id=models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True,editable=False)
@@ -20,9 +23,9 @@ class Buyer(models.Model):
     
 
 
-class Order(models.Model):
-    buyer=models.OneToOneField(Buyer, on_delete=models.CASCADE, null=True, blank=True)
-    Product= models.OneToOneField(Product,on_delete=models.CASCADE, null=True, blank=True)
+
+class Product(models.Model):
+    username= models.ForeignKey(Seller,on_delete=models.CASCADE, null=True, blank=True)
     product_name=models.CharField(max_length=200,blank=True, null=True)
     catagory=models.CharField(max_length=200,blank=True, null=True)
     stock= models.IntegerField(max_length=455, blank=True, null=True)
@@ -34,8 +37,4 @@ class Order(models.Model):
     created=models.DateTimeField(auto_now=True)
     id=models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True,editable=False)
     def __str__(self):
-        return str(self.username)
-    
-
-
-    
+        return str(self.product_name)
