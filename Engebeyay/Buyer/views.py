94a494faf92fc34484161c306b9e6e055b2efd2f
AmirEth <amirdulla11@gmail.com>
2023-03-02 14:21:51 +0300

    1st day of working on autentication

--- Engebeyay/Buyer/views.py
+++ Engebeyay/Buyer/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+def home(request):
+
+    return render(request, 'index.html')
+
+
+def signin(request):
+
+    return render(request, "signin.html")
