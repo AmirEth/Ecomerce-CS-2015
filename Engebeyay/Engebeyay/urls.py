7c70d5d04235d6d241c3d860eec36d327298b466
AmirEth <amirdulla11@gmail.com>
2023-03-09 21:57:42 +0300

    project updated

--- Engebeyay/Engebeyay/urls.py
+++ Engebeyay/Engebeyay/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Buyer.urls')),
-]
+    path('', views.home, name='home'),
+    path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
+
+    # ORDERS
+    path('orders/', include('orders.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
