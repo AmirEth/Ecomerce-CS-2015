b4243790ed310a7cafdbd1408c8fd3bfd78d0b14
AmirEth <amirdulla11@gmail.com>
2023-03-10 18:20:33 +0300

    secured

--- Engebeyay/Engebeyay/settings.py
+++ Engebeyay/Engebeyay/settings.py
@@ -143,6 +143,6 @@ MESSAGE_TAGS = {
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'amirdulla11@gmail.com'
-EMAIL_HOST_PASSWORD = '24434amir'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
